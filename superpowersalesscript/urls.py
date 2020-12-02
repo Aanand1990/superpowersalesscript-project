44695f1b3b567e1c5b24505eb91b7028260a5459
Aanand Manohar <me@aanandmanohar.com>
2020-12-02 17:06:43 +0530

    changed home page

--- superpowersalesscript/urls.py
+++ superpowersalesscript/urls.py
@@ -15,13 +15,14 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
-from salesscripts import views
+from salesscripts import views as vs
+from accounts import views as va
 from django.conf.urls.static import static
 from django.conf import settings
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', views.home, name = 'home'),
+    path('', va.login, name = 'home'),
     path('accounts/', include('accounts.urls')),
     path('salesscripts/', include('salesscripts.urls')),
 
